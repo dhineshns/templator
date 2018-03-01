6d93ee5ac2b5c77baff61fafecfa9a24e186d94f
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-01 16:08:51 -0600

    Requirement 4 : When given an abosolute file name reuturn the file's contents

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def replace_string(template, string_to_be_replaced, replacing_string):
     return template.replace(string_to_be_replaced, replacing_string)
 
 def gen_text_from_template(template, replaced_to_replacing_map):
-    for replaced, replacing in replaced_to_replacing_map.iteritems():
+    for replaced, replacing in replaced_to_replacing_map.items():
         template = replace_string(template, replaced, replacing)
     return template
 
+def get_string_from_file_external_dep(filename):
+    return open(filename, "r").read()
--- test_main.py
+++ test_main.py
@@ -3,8 +3,6 @@ import main as main
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
     ## Test templator
     ## Requirement 1 :  Input of empty string should return empty string
     def test_replace_string_empty_string(self):
@@ -13,17 +11,25 @@ class TestStringMethods(unittest.TestCase):
 
     ## Requirement 2 : Input of template, string to be replaced, replacing_string will return the generate text.
     def test_replace_string_replacing_string(self):
-        template = "Please return the <<<BOOK_NAME>> book. Again the books name is <<<BOOK_NAME>>"
-        string_to_be_replaced = "<<<BOOK_NAME>>"
+        template = "Please return the <<<BOOK_NAME>>> book. Again the books name is <<<BOOK_NAME>>>"
+        string_to_be_replaced = "<<<BOOK_NAME>>>"
         replacing_string = "Outliers"
         result = main.replace_string(template, string_to_be_replaced, replacing_string)
         self.assertEqual(result, "Please return the Outliers book. Again the books name is Outliers")
 
     ## Requirement 3 : Input of template, map of replaced and replacing string would return replaced outputs.
     def test_gen_text_valid_inputs(self):
-        template = "Hello <<<NAME>>>, Please return the <<<BOOK_NAME>> book."
-        replacing_to_replaced_map = {"<<<NAME>>>": "Dhinesh", "<<<BOOK_NAME": "Outliers"}
+        template = "Hello <<<NAME>>>, Please return the <<<BOOK_NAME>>> book."
+        replacing_to_replaced_map = {"<<<NAME>>>": "Dhinesh", "<<<BOOK_NAME>>>": "Outliers"}
         result = main.gen_text_from_template(template, replacing_to_replaced_map)
         self.assertEqual("Hello Dhinesh, Please return the Outliers book.", result)
+
+    ## Requirement 4 : When given an abosolute file name reuturn the file's contents
+    def test_get_string_from_file_external_dep(self):
+        file_name = "template.txt"
+        result = main.get_string_from_file_external_dep(file_name)
+        expected = "Hi <<<TO_NAME>>>, Please return the <<<BOOK_NAME>> book, that you borrowed on <<<DATE>>>. Thank you so much <<<TO_NAME>>>. -Dhinesh"
+        self.assertEqual(result, expected)
+
 if __name__ == '__main__':
     unittest.main()
