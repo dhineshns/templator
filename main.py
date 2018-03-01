1602523f5e2e5514a9757ff4581d58cfd453ad42
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-01 15:50:15 -0600

    Red Green Refactor : Requirement 2 : Input of template, map of replaced and replacing string would return replaced outputs.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def gen_text_from_template(template, string_to_be_replaced, replacing_string):
+def replace_string(template, string_to_be_replaced, replacing_string):
     return template.replace(string_to_be_replaced, replacing_string)
+
+def gen_text_from_template(template, replaced_to_replacing_map):
+    for replaced, replacing in replaced_to_replacing_map.iteritems():
+        template = replace_string(template, replaced, replacing)
+    return template
+
--- test_main.py
+++ test_main.py
@@ -7,17 +7,23 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
     ## Test templator
     ## Requirement 1 :  Input of empty string should return empty string
-    def test_gen_text_from_template_empty_string(self):
-        result = main.gen_text_from_template("", "", "")
+    def test_replace_string_empty_string(self):
+        result = main.replace_string("", "", "")
         self.assertEqual(result, "")
 
     ## Requirement 2 : Input of template, string to be replaced, replacing_string will return the generate text.
-    def test_gen_text_from_template_replacing_string(self):
+    def test_replace_string_replacing_string(self):
         template = "Please return the <<<BOOK_NAME>> book. Again the books name is <<<BOOK_NAME>>"
         string_to_be_replaced = "<<<BOOK_NAME>>"
         replacing_string = "Outliers"
-        result = main.gen_text_from_template(template, string_to_be_replaced, replacing_string)
+        result = main.replace_string(template, string_to_be_replaced, replacing_string)
         self.assertEqual(result, "Please return the Outliers book. Again the books name is Outliers")
 
+    ## Requirement 3 : Input of template, map of replaced and replacing string would return replaced outputs.
+    def test_gen_text_valid_inputs(self):
+        template = "Hello <<<NAME>>>, Please return the <<<BOOK_NAME>> book."
+        replacing_to_replaced_map = {"<<<NAME>>>": "Dhinesh", "<<<BOOK_NAME": "Outliers"}
+        result = main.gen_text_from_template(template, replacing_to_replaced_map)
+        self.assertEqual("Hello Dhinesh, Please return the Outliers book.", result)
 if __name__ == '__main__':
     unittest.main()
