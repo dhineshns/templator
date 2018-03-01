b147fee777d5945a6894c9ddd49bf55d668f13e6
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-01 15:37:34 -0600

    Red Green Refactor : Requirement 2 : Input of template, string to be replaced, replacing_string will return the generate text

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-def gen_text_from_template(template):
-    return ""
+def gen_text_from_template(template, string_to_be_replaced, replacing_string):
+    return template.replace(string_to_be_replaced, replacing_string)
--- test_main.py
+++ test_main.py
@@ -7,9 +7,17 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
     ## Test templator
     ## Requirement 1 :  Input of empty string should return empty string
-    def test_gen_text_from_template(self):
-        result = main.gen_text_from_template("")
+    def test_gen_text_from_template_empty_string(self):
+        result = main.gen_text_from_template("", "", "")
         self.assertEqual(result, "")
+
+    ## Requirement 2 : Input of template, string to be replaced, replacing_string will return the generate text.
+    def test_gen_text_from_template_replacing_string(self):
+        template = "Please return the <<<BOOK_NAME>> book. Again the books name is <<<BOOK_NAME>>"
+        string_to_be_replaced = "<<<BOOK_NAME>>"
+        replacing_string = "Outliers"
+        result = main.gen_text_from_template(template, string_to_be_replaced, replacing_string)
+        self.assertEqual(result, "Please return the Outliers book. Again the books name is Outliers")
 
 if __name__ == '__main__':
     unittest.main()
